e2d73fba6f1f6d5022162f88602990d53caafdd7
chrkaw152 <114194285+chrkaw152@users.noreply.github.com>
2023-06-09 16:37:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const app = express();
+app.use(express.static("./private"));
 
+app.get("/", function(req, res) {
+  res.sendFile(_dirname + "/index.html");
+});
+app.post("/", function(req, res) {
+  res.sendFile(_dirname + "/index2.html");
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running on port ${process.env.PORT}.");
+}
